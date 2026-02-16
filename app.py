2692e85d35f089cb1c558c7d76f4cef28a94c7fc
Abhishek Mishra <abhishek.mishra@prolifics.com>
2026-02-16 23:39:55 +0530

    Added req

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 st.set_page_config(
     page_title="HealthChat POC - Patient & Doctor",
-    page_icon="🩺",
+    page_icon="❤️",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
